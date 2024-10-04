8ea66309fd45401c6927c7eb60525028e7698017
surecam-01 <surecamapitest@gmail.com>
2024-10-04 14:04:14 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,25 +8,24 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       constraints format: :json do
-
-        match '/', :to => "api#index", :via => :all
+        
         match '/index', :to => "api#index", :via => :all
 
         match "/register", :to => "users#new", :via => :get
-        match "/register", :to => "users#create", :via => :post
+        match "/register", :to => "users#create", :via => [:put, :post]
 
         match '/users',     :to => "users#index", :via => :get
         match '/users/:id', :to => "users#show", :via => :get
         match '/users',     :to => "users#destroy", :via => :delete
 
         match "/login",  :to => "sessions#new", :via => :get
-        match "/login",  :to => "sessions#create", :via => :post
+        match "/login",  :to => "sessions#create", :via => [:put, :post]
         match "/logout", :to => "sessions#destroy", :via => :delete
   
-        match "/posts", :to => "posts#create", :via => :post
+        match "/posts",     :to => "posts#create", :via => [:put, :post]
         match "/posts/:id", :to => "posts#destroy", :via => :delete
 
-        match "/comments", :to => "comments#create", :via => :post
+        match "/comments",     :to => "comments#create", :via => [:put, :post]
         match "/comments/:id", :to => "comments#destroy", :via => :delete
 
         match '/todos', :to => "todos#index", :via => :get
